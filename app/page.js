140766051563cea6a5272fcb5425c37cf3805ebb
Josh May <joshmay222@gmail.com>
2024-01-27 16:58:26 -0600

    lazy git

--- app/page.js
+++ app/page.js
@@ -1,113 +1,93 @@
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="flex flex-col items-center justify-center ">
+      <Head>
+        <title>Austin Toastmasters - The Junto</title>
+        <meta
+          name="description"
+          content="A place where normies practice public speaking."
         />
-      </div>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <nav className="w-full bg-[#171717] border-b-2 border-zinc-700 p-4 flex justify-between items-center">
+        <div className="flex items-center ml-1">
+          <Image
+            src="/tm2.png"
+            alt="Austin Toastmasters Logo"
+            width={60}
+            height={60}
+          />
+          <span className="text-white ml-3 font-semibold">
+            Austin Toastmasters
+          </span>
+        </div>
+        <ul className="flex justify-center space-x-8 mr-10">
+          <li>
+            <a href="#" className="text-white no-underline hover:underline">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white no-underline hover:underline">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white no-underline hover:underline">
+              FAQs
+            </a>
+          </li>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <li>
+            <a href="#" className="text-white no-underline hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+      <main className="flex flex-1 flex-col  items-center justify-center rounded-md ">
+        <div className="mb-7">
+          <h1 className=" mb-1  mt-[45px] text-center text-4xl font-bold ">
+            Welcome to Austin Toastmasters!
+          </h1>
+          <p className="text-lg text-[#FFF8EE] ">
+            Guests are welcome to drop in and attend meetings without notice.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        </div>
+        <p className="text-left w-full font-bold mb-1">MEETING INFO</p>
+        <div className=" w-full border p-3">
+          <p className="text-lg leading-7 text-[#FFF8EE]">
+            Club Name:{" "}
+            <a
+              href="https://en.wikipedia.org/wiki/Junto_(club)"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-400 underline"
+            >
+              The Junto
+            </a>
           </p>
-        </a>
-      </div>
-    </main>
+          <p className="text-lg leading-7 text-[#FFF8EE]">
+            Location:{" "}
+            <a
+              href="https://www.google.com/maps/place/Createscape+Coworking/@30.2601314,-97.7041995,15z/data=!4m2!3m1!1s0x0:0xd27a2b1184f9b146?sa=X&ved=2ahUKEwiVzcjx0v6DAxXHlGoFHaLiDy8Q_BJ6BAhrEAA"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-400 underline"
+            >
+              Createscape Coworking
+            </a>
+          </p>
+          <p className="text-lg leading-7 text-[#FFF8EE] ">
+            Time: Thursday @ 5:15pm
+          </p>
+        </div>
+      </main>
+    </div>
   );
 }
