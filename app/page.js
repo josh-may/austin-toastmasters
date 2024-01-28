d4b60b09593826d250a618ed5af5f56e20b1a9eb
Josh May <joshmay222@gmail.com>
2024-01-28 13:59:14 -0600

    lazy git

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center ">
       <Head>
-        <title>Austin Toastmasters - The Junto</title>
+        <title>Austin Toastmasters - West Austin I Toastmasters Club</title>
         <meta
           name="description"
           content="A place where normies practice public speaking."
@@ -13,48 +13,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className="w-full bg-[#171717] border-b-2 border-zinc-700 p-4 flex justify-between items-center">
-        <div className="flex items-center ml-1">
-          <Image
-            src="/tm2.png"
-            alt="Austin Toastmasters Logo"
-            width={60}
-            height={60}
-          />
-          <span className="text-white ml-3 font-semibold">
-            Austin Toastmasters
-          </span>
-        </div>
-        <ul className="flex justify-center space-x-8 mr-10">
-          <li>
-            <a href="#" className="text-white no-underline hover:underline">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white no-underline hover:underline">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white no-underline hover:underline">
-              FAQs
-            </a>
-          </li>
-
-          <li>
-            <a href="#" className="text-white no-underline hover:underline">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
 
       <main className="flex flex-1 flex-col  items-center justify-center rounded-md ">
         <div className="mb-7">
           <h1 className=" mb-1  mt-[45px] text-center text-4xl font-bold ">
             Welcome to Austin Toastmasters!
-          </h1>
+          </h1>{" "}
           <p className="text-lg text-[#FFF8EE] ">
             Guests are welcome to drop in and attend meetings without notice.
           </p>
@@ -64,27 +29,27 @@ export default function Home() {
           <p className="text-lg leading-7 text-[#FFF8EE]">
             Club Name:{" "}
             <a
-              href="https://en.wikipedia.org/wiki/Junto_(club)"
+              href="https://www.toastmasters.org/Find-a-Club/00005531-west-austin-i-toastmasters-club"
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-400 underline"
             >
-              The Junto
+              West Austin I Toastmasters Club
             </a>
           </p>
           <p className="text-lg leading-7 text-[#FFF8EE]">
             Location:{" "}
             <a
-              href="https://www.google.com/maps/place/Createscape+Coworking/@30.2601314,-97.7041995,15z/data=!4m2!3m1!1s0x0:0xd27a2b1184f9b146?sa=X&ved=2ahUKEwiVzcjx0v6DAxXHlGoFHaLiDy8Q_BJ6BAhrEAA"
+              href="https://us02web.zoom.us/j/590258400?pwd=V0g3TWNXcDhXUmZRT3RkcDNnMzdZQT09"
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-400 underline"
             >
-              Createscape Coworking
+              Zoom
             </a>
           </p>
           <p className="text-lg leading-7 text-[#FFF8EE] ">
-            Time: Thursday @ 5:15pm
+            Time: Friday 7:30 am - 8:30 am
           </p>
         </div>
       </main>
---
+++ components/navbar.js
@@ -0,0 +1,62 @@
+import Image from "next/image";
+
+const Navbar = () => (
+  <nav className="w-full bg-[#171717] border-b-2 border-zinc-700 p-4 flex justify-between items-center">
+    <div className="flex items-center ml-1">
+      <Image
+        src="/tm2.png"
+        alt="Austin Toastmasters Logo"
+        width={60}
+        height={60}
+      />
+      <a href="/" className="text-white ml-3 font-semibold">
+        Austin Toastmasters
+      </a>
+    </div>
+    <ul className="flex justify-center space-x-8 mr-10">
+      <li>
+        <a href="/" className="text-white no-underline hover:underline">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="/about" className="text-white no-underline hover:underline">
+          About
+        </a>
+      </li>
+      <li>
+        <a href="#" className="text-white no-underline hover:underline">
+          FAQs
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="mailto:WestAustinitoastmasters@gmail.com"
+          className="text-white no-underline hover:underline"
+        >
+          Contact
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.facebook.com/groups/westaustintoastmasters/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/fb.png" alt="Facebook" width={30} height={30} />
+        </a>
+      </li>
+      <li>
+        <a
+          href="tel:512-789-0900"
+          className="text-white no-underline hover:underline"
+        >
+          512-789-0900
+        </a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
